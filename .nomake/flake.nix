e2f29586c0b80eeb46ea0ddc38e247eea6ccfd60
Akira Komamura <akira.komamura@gmail.com>
2023-05-21 18:08:38 +0900

    Lock

---
+++ .nomake/flake.nix
@@ -0,0 +1,13 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    flymake-bean-check = {
+      flake = false;
+      owner = "akirak";
+      repo = "flymake-bean-check";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
